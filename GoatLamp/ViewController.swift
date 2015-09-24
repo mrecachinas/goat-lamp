3f126a3ed6b421241cf2a21482efbb25963b9a97
Michael Recachinas <mgr3yp@virginia.edu>
2015-09-24 22:16:03 +0200

    Initial commit
    Basic structure and logic working. Adding images to test functionality.

--- GoatLamp/ViewController.swift
+++ GoatLamp/ViewController.swift
@@ -9,10 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var editBtn: UIButton!
+    var editEnabled: Bool = false
 
+    @IBOutlet weak var topNextBtn: UIButton!
+    @IBOutlet weak var topPrevBtn: UIButton!
+    @IBOutlet weak var midNextBtn: UIButton!
+    @IBOutlet weak var midPrevBtn: UIButton!
+    @IBOutlet weak var botNextBtn: UIButton!
+    @IBOutlet weak var botPrevBtn: UIButton!
+    
+    var topImageArray: [UIImage]!
+    var midImageArray: [UIImage]!
+    var botImageArray: [UIImage]!
+    
+    var topIdx: Int = 0
+    var midIdx: Int = 0
+    var botIdx: Int = 0
+    
+    @IBOutlet weak var topImageView: UIImageView!
+    @IBOutlet weak var midImageView: UIImageView!
+    @IBOutlet weak var botImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        hideEditBtns()
+        
+        var img1: UIImage = UIImage(contentsOfFile: "top1.jpg")!
+        var img2: UIImage = UIImage(contentsOfFile: "top2.jpg")!
+        var img3: UIImage = UIImage(contentsOfFile: "top3.jpg")!
+        topImageArray = [img1, img2, img3]
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +47,76 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func hideEditBtns() {
+        topNextBtn.hidden = true
+        topPrevBtn.hidden = true
+        midNextBtn.hidden = true
+        midPrevBtn.hidden = true
+        botNextBtn.hidden = true
+        botPrevBtn.hidden = true
+    }
+    
+    func showEditBtns() {
+        topNextBtn.hidden = false
+        topPrevBtn.hidden = false
+        midNextBtn.hidden = false
+        midPrevBtn.hidden = false
+        botNextBtn.hidden = false
+        botPrevBtn.hidden = false
+    }
+    
+    @IBAction func enableEdit(sender: UIButton) {
+        if editEnabled {
+            hideEditBtns()
+            editBtn.setTitle("Edit", forState: UIControlState.Normal)
+        } else {
+            showEditBtns()
+            editBtn.setTitle("Done", forState: UIControlState.Normal)
+        }
+        editEnabled = !editEnabled
+    }
+    
+    @IBAction func nextAction(sender: UIButton) {
+        switch(sender) {
+            case topNextBtn:
+                topIdx = (topIdx + 1) % topImageArray.count
+                updateImage(topIdx, imageView: topImageView, imageArray: topImageArray)
+                break
+            case midNextBtn:
+                midIdx = (midIdx + 1) % midImageArray.count
+                updateImage(midIdx, imageView: midImageView, imageArray: midImageArray)
+                break
+            case botNextBtn:
+                botIdx = (botIdx + 1) % botImageArray.count
+                updateImage(botIdx, imageView: botImageView, imageArray: botImageArray)
+                break
+            default:
+                break
+        }
+    }
 
+    @IBAction func prevAction(sender: UIButton) {
+        switch(sender) {
+            case topNextBtn:
+                topIdx = (topIdx - 1) % topImageArray.count
+                updateImage(topIdx, imageView: topImageView, imageArray: topImageArray)
+                break
+            case midNextBtn:
+                midIdx = (midIdx - 1) % midImageArray.count
+                updateImage(midIdx, imageView: midImageView, imageArray: midImageArray)
+                break
+            case botNextBtn:
+                botIdx = (botIdx - 1) % botImageArray.count
+                updateImage(botIdx, imageView: botImageView, imageArray: botImageArray)
+                break
+            default:
+                break
+        }
+    }
+    
+    func updateImage(idx: Int, imageView: UIImageView, imageArray: [UIImage]) {
+        imageView.image = imageArray[idx]
+    }
 }
 
